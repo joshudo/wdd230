f338c34be39b0931f63eb4bdfc8dd62655eae103
joshudo <joshua.d.ortiz93@gmail.com>
2022-09-23 21:37:22 -0600

    updating new folder

---
+++ lesson2/scripts/last-modified.js
@@ -0,0 +1,10 @@
+const options = {
+    weekday: 'long',
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric'
+}
+
+let oLastModif = new Date(document.lastModified);
+
+document.getElementById("last-modified").textContent = oLastModif.toLocaleString("en-US", options);
