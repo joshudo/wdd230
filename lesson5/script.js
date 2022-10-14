0ace7864814ea3ec2222b5715d6e112099073915
joshudo <joshua.d.ortiz93@gmail.com>
2022-10-13 22:47:37 -0600

    updating lesson 5

---
+++ lesson5/script.js
@@ -0,0 +1,27 @@
+const input = document.querySelector('input');
+const button = document.querySelector('button');
+const list = document.querySelector('ul');
+
+button.addEventListener('click', function (){
+    
+    let listItem = document.createElement('li');
+    let deleteBtn = document.createElement('button');
+    let listContent = document.createElement('p');
+
+    listItem.append(listContent);
+    listContent.textContent = input.value;
+    listItem.append(deleteBtn);
+    deleteBtn.textContent = "X";
+
+    list.append(listItem);
+
+    deleteBtn.addEventListener('click', function(){
+        list.removeChild(listItem);
+    });
+    
+    input.value = ""
+
+    input.focus();
+
+    }
+)
