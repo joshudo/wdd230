5bd9c991768b2bfbaaa0654b1929cbd6c59eafa9
joshudo <joshua.d.ortiz93@gmail.com>
2022-10-21 23:13:23 -0600

    updating windchill

--- chamber1/js/windchill.js
+++ chamber1/js/windchill.js
@@ -1,7 +1,5 @@
 var fTempNum = parseFloat(document.getElementById('temp-c').textContent);
 
-/*var fTempNum = cTempNum*1.8+32*/
-
 var speed = parseFloat(document.getElementById('w-speed').textContent);
 
 var result = (35.74 + 0.6215*fTempNum - 35.75*(speed**0.16) + 0.4275*fTempNum*(speed**0.16)).toFixed(0);
