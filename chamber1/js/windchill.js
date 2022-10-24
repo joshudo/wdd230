4d184dd75590c8ef5b7af1ef6c7b7807511b0312
joshudo <joshua.d.ortiz93@gmail.com>
2022-10-24 15:10:28 -0600

    updating values

--- chamber1/js/windchill.js
+++ chamber1/js/windchill.js
@@ -6,12 +6,7 @@ var result = (35.74 + 0.6215*fTempNum - 35.75*(speed**0.16) + 0.4275*fTempNum*(s
 var result = result + '°F'
 
 if(fTempNum <= 50 && speed > 3.0) {
-    document.getElementById('windchill-value').append(result);
+    document.getElementById('windchill-value').textContent = result;
 } else {
-    document.getElementById('windchill-value').append('N/A');
+    document.getElementById('windchill-value').textContent = 'N/A';
 }
-
-
-
-console.log(result);
-
