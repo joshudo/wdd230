fe9322b0721630cd3b01e0fdd6129cddc42e0b01
joshudo <joshua.d.ortiz93@gmail.com>
2022-10-23 23:58:53 -0600

    updating empty

--- lesson5/script.js
+++ lesson5/script.js
@@ -3,25 +3,28 @@ const button = document.querySelector('button');
 const list = document.querySelector('ul');
 
 button.addEventListener('click', function (){
-    
-    let listItem = document.createElement('li');
-    let deleteBtn = document.createElement('button');
-    let listContent = document.createElement('p');
+    if(input.value === ''){
+        alert('Please fill the required field');
+    } else {
+        let listItem = document.createElement('li');
+        let deleteBtn = document.createElement('button');
+        let listContent = document.createElement('p');
 
-    listItem.append(listContent);
-    listContent.textContent = input.value;
-    listItem.append(deleteBtn);
-    deleteBtn.textContent = "X";
+        listItem.append(listContent);
+        listContent.textContent = input.value;
+        listItem.append(deleteBtn);
+        deleteBtn.textContent = "X";
 
-    list.append(listItem);
+        list.append(listItem);
 
-    deleteBtn.addEventListener('click', function(){
-        list.removeChild(listItem);
-    });
-    
-    input.value = ""
+        deleteBtn.addEventListener('click', function(){
+            list.removeChild(listItem);
+        });
+        
+        input.value = ""
 
-    input.focus();
+        input.focus();
+    }    
 
     }
 )
