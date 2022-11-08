50db2c538f87c105e50052142d19c90add578f81
joshudo <joshua.d.ortiz93@gmail.com>
2022-11-07 20:04:16 -0600

    comple lesson 9

--- chamber1/js/business.js
+++ chamber1/js/business.js
@@ -1,4 +1,45 @@
-const requestURL = "../json/data.json";
-const cards = document.querySelector('.cards');
+const requestData = "./json/data.json";
+const cards = document.querySelector('#cards');
 
-fetch
+fetch(requestData)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function (jsonObject){
+        console.table(jsonObject);
+        const companies = jsonObject['companies'];
+        companies.forEach(displayCompanies);
+    })
+
+function displayCompanies(companies) {
+    let card = document.createElement('section');
+    let h2 = document.createElement('h2');
+    let logo = document.createElement('img');
+    let url = document.createElement('a');
+    let founder = document.createElement('p');
+
+    h2.textContent = companies.name;
+
+    logo.setAttribute('src', companies.img);
+    logo.setAttribute('alt',`${companies.name} logo`);
+    logo.setAttribute('loading', 'lazy');
+
+    url.textContent = `Visit their Website`;
+    url.setAttribute('href', companies.url);
+    founder.textContent = `Founder: ${companies.founder}`;
+
+    card.appendChild(h2);
+    card.appendChild(logo);
+    card.appendChild(url);
+    card.appendChild(founder);
+
+    document.querySelector('div#cards').appendChild(card);
+}
+
+function toggleView() {
+    document.querySelector('#cards').classList.toggle('list');
+    document.querySelector("#list-grid-btn").classList.toggle('list');
+}
+
+const listgridBtn = document.querySelector('#list-grid-btn');
+listgridBtn.onclick = toggleView;
